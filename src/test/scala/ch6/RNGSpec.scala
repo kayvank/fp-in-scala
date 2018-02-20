45a3c63ba4f3b3eb9ac6d1f4b4a6902e7c22d9c1
Kayvan Kazeminejad <kayvan.kazeminejad@gmail.com>
2018-02-20 07:18:10 -0800

    add hash-table question

--- src/test/scala/ch6/RNGSpec.scala
+++ src/test/scala/ch6/RNGSpec.scala
@@ -1,6 +1,7 @@
 package ch6
 
 import org.specs2._
+import scala.collection.immutable._
 
 class RNGSpec extends Specification { def is = s2"""
 
@@ -9,6 +10,12 @@ Chapter 6 Purely Functional state, Random Number Generator
   specification tow $e2
   specification three $e3
 """
+  val input = " the quick brown fox jumped over the brown-foxed-chair"
+  def splitter(str: String): List[String] =  str.split("\\s+|-").toList
+  def freqCounter(words: List[String]): Map[String, Int] = {
+    new HashMap[String, Int]()
+  }
+    
   def e1 = 1 must_== 0
   def e2 = 1 must_== 0
   def e3 = 1 must_== 0
